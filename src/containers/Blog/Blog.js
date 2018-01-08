c7f6abf02153a4f50ee18fe548bb3d5d731e71dd
1paciente <1paciente.com@gmail.com>
2018-01-08 17:05:28 +0100

    deploy files

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
